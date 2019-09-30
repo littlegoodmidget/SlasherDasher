77193ed4ee38cca77188ea32ebea7ad132e556ae
littlegoodmidget <littlemidgetmc@gmail.com>
2019-09-30 20:31:38 +1300

    adding basic items

--- canvas.js
+++ canvas.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('canvas');
 canvas.width = 600;
 canvas.height = 600;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 const c = canvas.getContext('2d');
 const quadTree = new QuadSector(new Rect(0, 0, canvas.width, canvas.height));
 
@@ -8,3 +10,15 @@ const quadTree = new QuadSector(new Rect(0, 0, canvas.width, canvas.height));
 
 
 
+
+
+
+
+
+
+
+canvas.addEventListener('mousedown', function(e) {
+    let point = new Point(e.offsetX, e.offsetY);
+    quadTree.insert(point);
+    point.draw();
+})
--- classes/QuadSector.js
+++ classes/QuadSector.js
@@ -9,7 +9,7 @@ class Rect {
 
 class QuadSector {
     static MaxDepth = 6;
-    static MaxObjects = 3;
+    static MaxObjects = 2;
     constructor(dimensions, depth = 0) {
         this.depth = depth;
         this.dimensions = dimensions;
@@ -22,17 +22,69 @@ class QuadSector {
         this.BL = undefined;
         this.BR = undefined;
 
-
         this.draw();
     }
+    insertToChild(obj) {
+        let percentX = ((obj.x - this.dimensions.x) / this.dimensions.width) * 2;
+        let percentY = ((obj.y - this.dimensions.y) / this.dimensions.height) * 2;
+        
+        let pos = [
+            ~~percentX,
+            ~~percentY
+        ];
+        // debugger
+
+        if(pos[0]==0&&pos[1]==0) {
+            this.TL.insert(obj);
+        }
+        if(pos[0]==1&&pos[1]==0) {
+            this.TR.insert(obj);
+        }
+        if(pos[0]==0&&pos[1]==1) {
+            this.BL.insert(obj);
+        }
+        if(pos[0]==1&&pos[1]==1) {
+            this.BR.insert(obj);
+        }
+    }
     insert(obj) {
-        if(this.objects.length < QuadSector.MaxObjects) {
+        if((!this.subDivided&&this.objects.length+1 < QuadSector.MaxObjects)||this.depth==QuadSector.MaxDepth) {
             this.objects.push(obj);
         }   else {
-            
+            if(this.depth<QuadSector.MaxDepth&&!this.subDivided) {
+                this.subDivided = true;
+                this.TL = new QuadSector(new Rect(this.dimensions.x, this.dimensions.y, this.dimensions.width/2, this.dimensions.height/2), this.depth+1);
+                this.TR = new QuadSector(new Rect(this.dimensions.x + this.dimensions.width/2, this.dimensions.y, this.dimensions.width/2, this.dimensions.height/2), this.depth+1);
+                this.BL = new QuadSector(new Rect(this.dimensions.x, this.dimensions.y + this.dimensions.height/2, this.dimensions.width/2, this.dimensions.height/2), this.depth+1);
+                this.BR = new QuadSector(new Rect(this.dimensions.x + this.dimensions.width/2, this.dimensions.y + this.dimensions.height/2, this.dimensions.width/2, this.dimensions.height/2), this.depth+1);
+                this.insertToChild(obj);
+                for(let i = 0; i < this.objects.length; i++) {
+                    this.insertToChild(this.objects[i]);
+                }
+                this.objects.length = 0;
+            }   else if(this.depth<QuadSector.MaxDepth) {
+                this.insertToChild(obj);
+            }
         }
     }
     draw() {
         c.strokeRect(this.dimensions.x, this.dimensions.y, this.dimensions.width, this.dimensions.height);
+    }
+}
+
+
+
+
+//TESTING CLASSES
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, 1, 0, Math.PI*2, false);
+        c.fill()
     }
 }
