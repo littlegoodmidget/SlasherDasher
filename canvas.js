547555ccf9f41f73dfe1e126373fce930c7f5bfc
littlegoodmidget <littlemidgetmc@gmail.com>
2019-09-30 18:41:58 +1300

    start of quad

--- canvas.js
+++ canvas.js
@@ -2,5 +2,9 @@ const canvas = document.getElementById('canvas');
 canvas.width = 600;
 canvas.height = 600;
 const c = canvas.getContext('2d');
+const quadTree = new QuadSector(new Rect(0, 0, canvas.width, canvas.height));
+
+
+
 
 
--- classes/QuadSector.js
+++ classes/QuadSector.js
@@ -1,6 +1,38 @@
+class Rect {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.width = w;
+        this.height = h;
+    }
+}
+
 class QuadSector {
+    static MaxDepth = 6;
+    static MaxObjects = 3;
     constructor(dimensions, depth = 0) {
         this.depth = depth;
         this.dimensions = dimensions;
+
+        this.subDivided = false;
+        this.objects = [];
+
+        this.TL = undefined;
+        this.TR = undefined;
+        this.BL = undefined;
+        this.BR = undefined;
+
+
+        this.draw();
+    }
+    insert(obj) {
+        if(this.objects.length < QuadSector.MaxObjects) {
+            this.objects.push(obj);
+        }   else {
+            
+        }
+    }
+    draw() {
+        c.strokeRect(this.dimensions.x, this.dimensions.y, this.dimensions.width, this.dimensions.height);
     }
 }
